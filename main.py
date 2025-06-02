13713b41b2139ee053be8551d871ea6535181f49
NicholasDenholm <nicholas.denholm1@gmail.com>
2025-06-02 15:36:25 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import skimage.io as io
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage.color import rgb2gray
+from skimage.filters import sobel
+import cv2
+
+
+def main():
+    print("hello!")
+
+
+if __name__ == "__main__":
+    main()
